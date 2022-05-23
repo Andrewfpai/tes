3cd4db89d194f4a6d3790d5a9ad236f7c6f81b01
Andrewfpai <nicolasandrew.28@gmail.com>
2022-05-24 00:16:34 +0700

    Initial Commit

---
+++ birthday/birth.js
@@ -0,0 +1,7 @@
+function flame(){
+  let flame = document.getElementById("flame")
+  let wish = document.getElementById("wish")
+  flame.style.display = 'none'
+  wish.textContent = 'Semoga semuanya terkabulkan!'
+  wish.style.marginLeft = "10px"
+}
