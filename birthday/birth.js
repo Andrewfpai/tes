a3a68afc67ce5029b45aae4ff71f47f6a7d62e8a
Andrewfpai <nicolasandrew.28@gmail.com>
2022-05-24 17:03:20 +0700

    .gitignore is now working

--- birthday/birth.js
+++
@@ -1,7 +0,0 @@
-function flame(){
-  let flame = document.getElementById("flame")
-  let wish = document.getElementById("wish")
-  flame.style.display = 'none'
-  wish.textContent = 'Semoga semuanya terkabulkan!'
-  wish.style.marginLeft = "10px"
-}
